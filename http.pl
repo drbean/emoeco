c9855e9bfd69e60977cac0d1bb3d049dd4d478fc
drbean <drbean@freeshell.org>
2021-05-19 16:54:30 +0800

    /search/stream?tweet.fields=conversation_id&expansions=author_id&user.fields=created_at.not_working!

--- http.pl
+++ http.pl
@@ -5,7 +5,7 @@ my $loop = AnyEvent->condvar;
 
 my $B='';
 
-my $watcher = http_get "https://api.twitter.com/2/tweets?ids=769350178792800258"
+my $watcher = http_get "https://api.twitter.com/2/tweets/search/stream?tweet.fields=conversation_id&expansions=author_id&user.fields=created_at"
 	, headers => {"Authorization" => "Bearer $B"}
 	, sub {
    my ($body, $hdr) = @_;
