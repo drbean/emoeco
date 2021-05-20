c1ec24c05d0f945c1c1d0cdc0b825b56d4163ca4
drbean <drbean@freeshell.org>
2021-05-20 17:33:14 +0800

    delete,add.body.not_working!

--- http.pl
+++ http.pl
@@ -3,11 +3,19 @@ use AnyEvent::HTTP;
  
 my $loop = AnyEvent->condvar;
 
-my $B='';
+my $B='AAAAAAAAAAAAAAAAAAAAAKUUOgEAAAAAMWeLE6TKmaiWRf0P5iim8zTu7Zw%3DXBk8asJuXVq05Hun6E4zQELvQA6fDoebo9Cf0q2pBMdmSCK6vy';
 
-my $watcher = http_get "https://api.twitter.com/2/tweets/search/stream/rules"
-	, headers => {"Authorization" => "Bearer $B", "Content-type" => "application/json"}
-	, {"delete" => {"ids" => ["1394519411847954437"]}}
+my $watcher = http_post "https://api.twitter.com/2/tweets/search/stream/rules"
+	, headers => {"Authorization" => "Bearer $B"
+			, "Content-type" => "application/json"
+	}
+	, body =>
+	#'{"delete":{"ids":["1395276321266159619"]}}'
+	  '{
+	    "add": [
+	      {"value": "\uD83D\uDE01 lang:en", "tag": "😀"},
+	    ]
+	  }'
 	, sub {
    my ($body, $hdr) = @_;
  
@@ -18,4 +26,5 @@ my $watcher = http_get "https://api.twitter.com/2/tweets/search/stream/rules"
    }
 };
 
+# $watcher->push_write( json => {"delete" => {"ids" => ["1394519411847954437"]}});
 $loop->recv;
