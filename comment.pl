4812a68de8db7170937aef601e2d98cf4775ec6d
drbean <drbean@freeshell.org>
2021-06-03 16:55:35 +0800

    recursive_fetching_sub.arrows,date

--- comment.pl
+++ comment.pl
@@ -5,60 +5,52 @@ use warnings;
 
 use LWP::UserAgent;
 use JSON;
+use Encode qw/encode decode/;
 
 my $ua = LWP::UserAgent->new;
 
-my $VI='7SeqQu2Ckcc';
+my $VI='BvJ1BuEtZEo';
 my $KEY='';
 
 my $url = "https://youtube.googleapis.com/youtube/v3/commentThreads?videoId=$VI&part=id,snippet,replies&maxResults=100&key=$KEY";
 
 my ($req, $res, $content, $next );
 open(my $fh, ">>:encoding(UTF-8)", "$VI.txt") or die "No open $VI.txt? $!";
+$next = 1;
 
-$req = HTTP::Request->new(GET => $url);
-$res = $ua->request($req);
-if ( $res->is_success ) {
-	$content = decode_json $res->content;
-	$next = $content->{nextPageToken};
-	my $items = $content->{items};
-	for my $comment ( @$items ) {
-		my $top = $comment->{snippet}->{topLevelComment}->{snippet}->{textOriginal};
-		$top =~ s/\n/\\n/g;
-		print $fh $top . "\n";
-		if ( my $replies = $comment->{replies}->{comments} ) {
-			for my $reply ( @$replies ) {
-				my $response = $reply->{snippet}->{textOriginal};
-				print $fh "->\t$response\n";
-			}
-		}
-	}
-} else {
-	die "$url failed: $!";
-}
-
-while ( $next ) {
+sub fetching {
 	sleep 2;
-	$url = "https://youtube.googleapis.com/youtube/v3/commentThreads?videoId=$VI&part=id,snippet,replies&maxResults=100&key=$KEY&pageToken=$next";
-	$req = HTTP::Request->new(GET => $url);
+	my $next_url = $next>1? $url . "&pageToken=$content->{nextPageToken}" : $url;
+	$req = HTTP::Request->new(GET => $next_url);
 	$res = $ua->request($req);
 	if ( $res->is_success ) {
 		$content = decode_json $res->content;
-		$next = $content->{nextPageToken} ? $content->{nextPageToken} : 0;
+		$next++;
 		my $items = $content->{items};
 		for my $comment ( @$items ) {
-			my $top = $comment->{snippet}->{topLevelComment}->{snippet}->{textOriginal};
-			$top =~ s/\n/\\n/g;
-			print $fh $top . "\n";
+			my $data = $comment->{snippet}->{topLevelComment}->{snippet};
+			my $top = $data->{textOriginal};
+			my $line;
+			$top =~ s/\n/\x{21B2} /g;
+			$line = $top . "\n";
 			if ( my $replies = $comment->{replies}->{comments} ) {
+				$line = "$data->{publishedAt}  $data->{authorDisplayName}:\t$line";
+				my @reply_line;
 				for my $reply ( @$replies ) {
-					my $response = $reply->{snippet}->{textOriginal};
-					$response =~ s/\n/\\n/g;
-					print $fh "->\t$response\n";
+					my $data = $reply->{snippet};
+					my $response = $data->{textOriginal};
+					$response =~ s/\n/\x{21B2} /g;
+					unshift @reply_line, "\x{21B3} $data->{publishedAt} $data->{authorDisplayName}:\t$response\n";
 				}
+				$line = join "", $line, @reply_line;
 			}
+			my $out = encode( "utf-8", $line );
+			# print $fh $out;
+			print $fh $line;
 		}
 	} else {
 		die "$url failed on $next page: $!";
 	}
-	}
+	&fetching if $content->{nextPageToken};
+}
+&fetching;
